f6938edc316b7a67102c18adcece6caa569255b6
SALM0AN <enboland@gmail.com>
2022-08-03 16:05:37 +1200

    Paint component added

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,18 +1,23 @@
 import javax.swing.JPanel;
-
+import javax.swing.Timer;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	public PongPanel() {
 		
 		//Define Constant variables
 		final Color BACKGROUND_COLOUR = Color.BLACK;
+		final int TIMER_DELAY = 5;
 		//Set Attributes
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
 
 	@Override
@@ -35,8 +40,17 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		update();
 		
 	}
+	private void update() {
+		
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
 
+}
 }
