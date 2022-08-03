6c8bab6206e2033322f7e74549d1c0d2b745b02a
SALM0AN <enboland@gmail.com>
2022-08-03 15:17:41 +1200

    JFrame added and configured

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,23 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame {
+	
+	//Declare constant variables
+	private final int SCREEN_WIDTH = 800;
+	private final int SCREEN_HEIGHT = 600;
+	private final String GAME_TITLE = "Pong";
+	
+	public Pong() {
+		setTitle(GAME_TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub  aef
-		System.out.println("ballers");
+		new Pong();
 
 }
 }
